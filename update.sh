8d234f283a6def5fe2fb3856153d81f7c47950af
Ziv Rechnitser <ziv@devops.co.il>
2021-04-08 11:44:36 +0300

    Fix

--- update.sh
+++ update.sh
@@ -31,8 +31,8 @@ if [ ! -z ${PLUGIN_TASK_DEFINITION} ]; then
   ecs-deploy --region ${PLUGIN_AWS_REGION} --cluster ${PLUGIN_CLUSTER} --image ${PLUGIN_IMAGE_NAME} --task-definition ${PLUGIN_TASK_DEFINITION} $PLUGIN_SKIP_DEPLOYMENTS_CHECK
 fi
 
-if [ -z ${PLUGIN_PRE_SCRIPT_PATH} ]; then
-  bash PLUGIN_PRE_SCRIPT_PATH
+if [ -z "${PLUGIN_PRE_SCRIPT_PATH}" ]; then
+  bash "$PLUGIN_PRE_SCRIPT_PATH"
 fi
 
 ecs-deploy --region ${PLUGIN_AWS_REGION} --cluster ${PLUGIN_CLUSTER} --image ${PLUGIN_IMAGE_NAME} --service-name ${PLUGIN_SERVICE} --timeout ${PLUGIN_TIMEOUT} --min ${PLUGIN_MIN} --max ${PLUGIN_MAX} --enable-rollback $PLUGIN_SKIP_DEPLOYMENTS_CHECK
